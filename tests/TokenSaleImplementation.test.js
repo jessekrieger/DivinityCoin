595e9ba8453309b3a5efb671fa6e4f2492da70b8
kokosro <robert@kokos.ro>
2022-09-08 15:36:29 +0300

    sellExactAmount method

--- tests/TokenSaleImplementation.test.js
+++ tests/TokenSaleImplementation.test.js
@@ -14,7 +14,8 @@ describe('TokenSaleImplementation', () => {
   let MockERC20Token;
   let paymentToken;
   let divinityCoin;
-  let pricePerUnit;
+  let buyPricePerUnit;
+  let sellPricePerUnit;
   let Treasury;
   before(async () => {
     allAccounts = await ethers.getSigners();
@@ -27,31 +28,36 @@ describe('TokenSaleImplementation', () => {
     tokenSaleImplementation = await TokenSaleImplementation.deploy();
     paymentToken = await MockERC20Token.deploy('Payment Token', 'PAY', 6);
     divinityCoin = await MockERC20Token.deploy('Divinity Coin', 'DIVINE', 18);
-    pricePerUnit = ethers.utils.parseUnits('314.15', 6);
+    buyPricePerUnit = ethers.utils.parseUnits('314.15', 6);
+    sellPricePerUnit = ethers.utils.parseUnits('144', 6);
     await tokenSaleImplementation
       .initialize(
         divinityCoin.address,
         paymentToken.address,
         Treasury.address,
-        pricePerUnit,
+        buyPricePerUnit,
+        sellPricePerUnit,
       );
     const [
       contractDivinityCoin,
       contractPaymentToken,
       contractTreasury,
-      contractPricePerUnit,
+      contractBuyPricePerUnit,
+      contractSellPricePerUnit,
     ] = await Promise.all(
       [
         tokenSaleImplementation.DivinityCoin(),
         tokenSaleImplementation.PaymentToken(),
         tokenSaleImplementation.Treasury(),
-        tokenSaleImplementation.pricePerUnit(),
+        tokenSaleImplementation.buyPricePerUnit(),
+        tokenSaleImplementation.sellPricePerUnit(),
       ],
     );
     expect(contractDivinityCoin).to.equal(divinityCoin.address);
     expect(contractPaymentToken).to.equal(paymentToken.address);
     expect(contractTreasury).to.equal(Treasury.address);
-    expect(contractPricePerUnit).to.equal(pricePerUnit);
+    expect(contractBuyPricePerUnit).to.equal(buyPricePerUnit);
+    expect(contractSellPricePerUnit).to.equal(sellPricePerUnit);
   });
   describe('setConfigs', () => {
     it('should throw when trying to call from non-owner address', async () => {
@@ -61,11 +67,12 @@ describe('TokenSaleImplementation', () => {
           divinityCoin.address,
           paymentToken.address,
           Treasury.address,
-          pricePerUnit,
+          buyPricePerUnit,
+          sellPricePerUnit,
         )).to.be.revertedWith('Ownable: caller is not the owner');
     });
 
-    it('should only change pricePerUnit', async () => {
+    it('should only change buyPricePerUnit', async () => {
       const newPricePerUnit = ethers.utils.parseUnits('314.16', 6);
       await tokenSaleImplementation
         .setConfigs(
@@ -73,24 +80,28 @@ describe('TokenSaleImplementation', () => {
           ethers.constants.AddressZero,
           ethers.constants.AddressZero,
           newPricePerUnit,
+          ethers.BigNumber.from('0'),
         );
       const [
         contractDivinityCoin,
         contractPaymentToken,
         contractTreasury,
-        contractPricePerUnit,
+        contractBuyPricePerUnit,
+        contractSellPricePerUnit,
       ] = await Promise.all(
         [
           tokenSaleImplementation.DivinityCoin(),
           tokenSaleImplementation.PaymentToken(),
           tokenSaleImplementation.Treasury(),
-          tokenSaleImplementation.pricePerUnit(),
+          tokenSaleImplementation.buyPricePerUnit(),
+          tokenSaleImplementation.sellPricePerUnit(),
         ],
       );
       expect(contractDivinityCoin).to.equal(divinityCoin.address);
       expect(contractPaymentToken).to.equal(paymentToken.address);
       expect(contractTreasury).to.equal(Treasury.address);
-      expect(contractPricePerUnit).to.equal(newPricePerUnit);
+      expect(contractBuyPricePerUnit).to.equal(newPricePerUnit);
+      expect(contractSellPricePerUnit).to.equal(sellPricePerUnit);
     });
     it('should change Treasury address', async () => {
       await tokenSaleImplementation
@@ -99,39 +110,43 @@ describe('TokenSaleImplementation', () => {
           ethers.constants.AddressZero,
           otherAccounts[1].address,
           ethers.BigNumber.from('0'),
+          ethers.BigNumber.from('0'),
         );
       const [
         contractDivinityCoin,
         contractPaymentToken,
         contractTreasury,
-        contractPricePerUnit,
+        contractBuyPricePerUnit,
+        contractSellPricePerUnit,
       ] = await Promise.all(
         [
           tokenSaleImplementation.DivinityCoin(),
           tokenSaleImplementation.PaymentToken(),
           tokenSaleImplementation.Treasury(),
-          tokenSaleImplementation.pricePerUnit(),
+          tokenSaleImplementation.buyPricePerUnit(),
+          tokenSaleImplementation.sellPricePerUnit(),
         ],
       );
       expect(contractDivinityCoin).to.equal(divinityCoin.address);
       expect(contractPaymentToken).to.equal(paymentToken.address);
       expect(contractTreasury).to.equal(otherAccounts[1].address);
-      expect(contractPricePerUnit).to.equal(pricePerUnit);
+      expect(contractBuyPricePerUnit).to.equal(buyPricePerUnit);
+      expect(contractSellPricePerUnit).to.equal(sellPricePerUnit);
     });
   });
-  describe('getAmount', () => {
+  describe('getBuyAmount', () => {
     it('should return the correct amount of DivinityCoin for given payment amount', async () => {
       const paymentAmount = ethers.utils.parseUnits('1', 6);
       const expectedAmount = ethers.utils.parseUnits('0.003183192742320547', 18);
-      const amount = await tokenSaleImplementation.getAmount(paymentAmount);
+      const amount = await tokenSaleImplementation.getBuyAmount(paymentAmount);
       expect(amount).to.equal(expectedAmount);
     });
   });
-  describe('getCost', () => {
+  describe('getBuyCost', () => {
     it('should return the correct amount of PaymentToken for exact amount of DivinityCoin', async () => {
       const paymentAmount = ethers.utils.parseUnits('314.15', 6);
       const expectedAmount = ethers.utils.parseUnits('1', 18);
-      const amount = await tokenSaleImplementation.getCost(expectedAmount);
+      const amount = await tokenSaleImplementation.getBuyCost(expectedAmount);
       expect(amount).to.equal(paymentAmount);
     });
   });
@@ -168,6 +183,7 @@ describe('TokenSaleImplementation', () => {
           ethers.constants.AddressZero,
           ethers.constants.AddressZero,
           ethers.utils.parseUnits('10000000000000', 6),
+          ethers.BigNumber.from('0'),
         );
       const paymentAmount = ethers.utils.parseUnits('0.000001', 6);
       await paymentToken.connect(owner)
@@ -250,7 +266,7 @@ describe('TokenSaleImplementation', () => {
       const expectedDivinityAmount = ethers.BigNumber.from('10')
         .pow(18)
         .mul(paymentAmount)
-        .div(pricePerUnit);
+        .div(buyPricePerUnit);
 
       await expect(tokenSaleImplementation
         .connect(otherAccounts[0])
@@ -312,6 +328,7 @@ describe('TokenSaleImplementation', () => {
           ethers.constants.AddressZero,
           ethers.constants.AddressZero,
           ethers.utils.parseUnits('0.00001', 6),
+          ethers.BigNumber.from('0'),
         );
       const amount = ethers.utils.parseUnits('0.0000000000000001', 18);
       const divinityAmount = ethers.utils.parseUnits('100', 18);
@@ -409,6 +426,169 @@ describe('TokenSaleImplementation', () => {
       expect(await divinityCoin.balanceOf(Treasury.address)).to.equal(divinityAmount.sub(amount));
       expect(await paymentToken.balanceOf(Treasury.address)).to.equal(ethers.utils.parseUnits('314.15', 6));
       expect(await paymentToken.balanceOf(otherAccounts[0].address)).to.equal(ethers.utils.parseUnits('0', 6));
+    });
+  });
+  describe('getSellCost', () => {
+    it('should return correct resulting PaymentToken amount for selling 1 DivinityCoin', async () => {
+      const amountToSell = ethers.utils.parseUnits('1', 18);
+      const expectedCost = sellPricePerUnit;
+      const resultingCost = await tokenSaleImplementation.getSellCost(amountToSell);
+      expect(resultingCost).to.equal(expectedCost);
+    });
+    it('should return correct resulting PaymentToken amount for selling 2 DivinityCoin', async () => {
+      const amountToSell = ethers.utils.parseUnits('2', 18);
+      const expectedCost = sellPricePerUnit.mul(2);
+      const resultingCost = await tokenSaleImplementation.getSellCost(amountToSell);
+      expect(resultingCost).to.equal(expectedCost);
+    });
+  });
+  describe('sellExactAmount', () => {
+    it('should revert when trying to sell 0 amount', async () => {
+      await expect(tokenSaleImplementation
+        .connect(otherAccounts[0])
+        .sellExactAmount(ethers.BigNumber.from('0')))
+        .to.be.revertedWith('amount must be greater than 0');
+    });
+    it('should revert when trying to sell more than user balance', async () => {
+      await expect(tokenSaleImplementation
+        .connect(otherAccounts[0])
+        .sellExactAmount(ethers.utils.parseUnits('1', 18)))
+        .to.be.revertedWith('not enough tokens');
+    });
+    it('should revert when trying to sell more than TokenSale contract allowance', async () => {
+      await divinityCoin.connect(owner)
+        .mint(
+          otherAccounts[0].address,
+          ethers.utils.parseUnits('1', 18),
+        );
+      await expect(tokenSaleImplementation
+        .connect(otherAccounts[0])
+        .sellExactAmount(ethers.utils.parseUnits('1', 18)))
+        .to.be.revertedWith('not enough allowance');
+    });
+    it('should revert when resulting cost is zero', async () => {
+      const amountToSell = ethers.utils.parseUnits('0.00000000001', 18);
+      await divinityCoin.connect(owner)
+        .mint(
+          otherAccounts[0].address,
+          amountToSell,
+        );
+      await divinityCoin.connect(otherAccounts[0])
+        .approve(tokenSaleImplementation.address, amountToSell);
+      await tokenSaleImplementation.connect(owner)
+        .setConfigs(
+          ethers.constants.AddressZero,
+          ethers.constants.AddressZero,
+          ethers.constants.AddressZero,
+          ethers.BigNumber.from('0'),
+          ethers.utils.parseUnits('0.00001', 6),
+        );
+      await expect(tokenSaleImplementation
+        .connect(otherAccounts[0])
+        .sellExactAmount(amountToSell))
+        .to.be.revertedWith('resulting cost must be greater than 0');
+    });
+    it('should revert when Treasury does not have enough PaymentToken balance', async () => {
+      const amountToSell = ethers.utils.parseUnits('1', 18);
+      await divinityCoin.connect(owner)
+        .mint(
+          otherAccounts[0].address,
+          amountToSell,
+        );
+      await divinityCoin.connect(otherAccounts[0])
+        .approve(tokenSaleImplementation.address, amountToSell);
+      await expect(tokenSaleImplementation
+        .connect(otherAccounts[0])
+        .sellExactAmount(amountToSell))
+        .to.be.revertedWith('not enough payment tokens on Treasury');
+    });
+    it('should revert when TokenSale does not have enough PaymentToken allowance from Treasury', async () => {
+      const amountToSell = ethers.utils.parseUnits('1', 18);
+      await divinityCoin.connect(owner)
+        .mint(
+          otherAccounts[0].address,
+          amountToSell,
+        );
+      await divinityCoin.connect(otherAccounts[0])
+        .approve(tokenSaleImplementation.address, amountToSell);
+      await paymentToken.connect(owner)
+        .mint(
+          Treasury.address,
+          sellPricePerUnit,
+        );
+      await expect(tokenSaleImplementation
+        .connect(otherAccounts[0])
+        .sellExactAmount(amountToSell))
+        .to.be.revertedWith('not enough payment token allowance on Treasury');
+    });
+    it('should successfully sell 1 DivinityCoin', async () => {
+      const amountToSell = ethers.utils.parseUnits('1', 18);
+      await divinityCoin.connect(owner)
+        .mint(
+          otherAccounts[0].address,
+          amountToSell,
+        );
+      await divinityCoin.connect(otherAccounts[0])
+        .approve(tokenSaleImplementation.address, amountToSell);
+      await paymentToken.connect(owner)
+        .mint(
+          Treasury.address,
+          sellPricePerUnit,
+        );
+      await paymentToken.connect(Treasury)
+        .approve(tokenSaleImplementation.address, sellPricePerUnit);
+      await expect(tokenSaleImplementation
+        .connect(otherAccounts[0])
+        .sellExactAmount(amountToSell))
+        .to
+        .emit(tokenSaleImplementation, 'SellOrder')
+        .withArgs(
+          otherAccounts[0].address,
+          divinityCoin.address,
+          paymentToken.address,
+          amountToSell,
+          sellPricePerUnit,
+          sellPricePerUnit,
+        );
+
+      expect(await divinityCoin.balanceOf(otherAccounts[0].address)).to.equal(ethers.utils.parseUnits('0', 18));
+      expect(await divinityCoin.balanceOf(Treasury.address)).to.equal(ethers.utils.parseUnits('1', 18));
+      expect(await paymentToken.balanceOf(otherAccounts[0].address)).to.equal(sellPricePerUnit);
+      expect(await paymentToken.balanceOf(Treasury.address)).to.equal(ethers.utils.parseUnits('0', 6));
+    });
+    it('should successfully sell 2 DivinityCoin', async () => {
+      const amountToSell = ethers.utils.parseUnits('2', 18);
+      await divinityCoin.connect(owner)
+        .mint(
+          otherAccounts[0].address,
+          amountToSell,
+        );
+      await divinityCoin.connect(otherAccounts[0])
+        .approve(tokenSaleImplementation.address, amountToSell);
+      await paymentToken.connect(owner)
+        .mint(
+          Treasury.address,
+          sellPricePerUnit.mul(2),
+        );
+      await paymentToken.connect(Treasury)
+        .approve(tokenSaleImplementation.address, sellPricePerUnit.mul(2));
+      await expect(tokenSaleImplementation
+        .connect(otherAccounts[0])
+        .sellExactAmount(amountToSell))
+        .to
+        .emit(tokenSaleImplementation, 'SellOrder')
+        .withArgs(
+          otherAccounts[0].address,
+          divinityCoin.address,
+          paymentToken.address,
+          amountToSell,
+          sellPricePerUnit.mul(2),
+          sellPricePerUnit,
+        );
+      expect(await divinityCoin.balanceOf(otherAccounts[0].address)).to.equal(ethers.utils.parseUnits('0', 18));
+      expect(await divinityCoin.balanceOf(Treasury.address)).to.equal(ethers.utils.parseUnits('2', 18));
+      expect(await paymentToken.balanceOf(otherAccounts[0].address)).to.equal(sellPricePerUnit.mul(2));
+      expect(await paymentToken.balanceOf(Treasury.address)).to.equal(ethers.utils.parseUnits('0', 6));
     });
   });
 });
