bedd54bc080160d1db342b45be50ba3c38582990
kokosro <robert@kokos.ro>
2022-09-28 21:18:18 +0300

    set-treasury script

---
+++ scripts/set-configs.js
@@ -0,0 +1,34 @@
+const hre = require('hardhat');
+const question = require('../utils/question');
+
+const { ethers } = hre;
+
+const run = async () => {
+  const [owner] = await ethers.getSigners();
+  const artifact = await hre.artifacts.readArtifact('TokenSaleImplementation');
+  const tokenSaleAddress = await question.ask('TokenSale address: ');
+  const tokenSale = new ethers.Contract(tokenSaleAddress, artifact.abi, owner);
+
+  const treasuryAddress = await question.ask('Treasury address: ');
+
+  const tx = await tokenSale
+    .setConfigs(
+      ethers.constants.AddressZero,
+      ethers.constants.AddressZero,
+      treasuryAddress,
+      ethers.BigNumber.from('0'),
+      ethers.BigNumber.from('0'),
+      { gasLimit: 1000000 },
+    );
+
+  console.log(`changing treasury address to: ${treasuryAddress} ${tx.hash}`);
+
+  await tx.wait();
+};
+
+run().then(() => {
+  console.log('DONE');
+}).catch((e) => {
+  console.log(e);
+  process.exit(1);
+});
